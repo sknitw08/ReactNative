21092c117d9ac7be6ff2f5b3ef79718c0ec659dd
Sanjay Kumar <sk.nitw08@gmail.com>
2020-06-16 16:00:08 +0530

    Notifications, Social Sharing, Picking Image

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, StyleSheet, Modal, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, StyleSheet, Modal, Button, Alert, PanResponder, Share } from 'react-native';
 
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
@@ -53,6 +53,16 @@ function RenderDish(props) {
         }
     })
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     if (dish != null) {
         return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000} ref={this.handleViewRef} {...panResponder.panHandlers}>
@@ -79,6 +89,15 @@ function RenderDish(props) {
                         type='font-awesome'
                         color='#512DA8'
                         onPress={() => props.toggleCommentModal()}
+                    />
+                    <Icon
+                        raised
+                        reverse
+                        name='share'
+                        type='font-awesome'
+                        color='#51D2A8'
+                        style={styles.cardItem}
+                        onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
                     />
                 </View>
             </Card>
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -4,6 +4,8 @@ import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 
 import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
 
@@ -29,12 +31,15 @@ class Reservation extends Component {
             [
                 {
                     text:'Cancel',
-                    onPress:()=>this.resetForm(),
+                    onPress:()=> this.resetForm(),
                     style:'cancel'
                 },
                 {
                     text:'OK',
-                    onPress:()=>this.resetForm()
+                    onPress:()=> {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
                 }
             ],
             {cancelable:false}
@@ -46,6 +51,33 @@ class Reservation extends Component {
             guests: 1,
             smoking: false,
             date: ''
+        });
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound:true,
+                vibrate:true,
+                color: '#512DA8'
+            }
         });
     }
     
